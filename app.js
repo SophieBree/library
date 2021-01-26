8c0f790d27a2ba7254ac6425fb95f5b14a936392
Sophie Lewis <sophielewis@protonmail.com>
2021-01-27 00:16:20 +1100

    changed to row based design

--- app.js
+++ app.js
@@ -14,21 +14,33 @@ myLibrary.push(book1.title, book1.author);
 
 
 function addBookToLibrary() {
+   /* make myLibrary empty at the start of the function */
     myLibrary = [];
+    /* create the Book object and assign the book's title and author to the array */
     let tempBook = Object.create(Book);
     tempBook.title = document.querySelector('input[name="title"]');
     tempBook.author = document.querySelector('input[name="author"]');
     myLibrary.push(tempBook.title.value, tempBook.author.value);
-    const container = document.querySelector('#container');
-    let newBookSlot = document.createElement('div');
-    newBookSlot.classList.add('book');
+    /* declare container variables */
+    const titleContainer = document.querySelector('#titles');
+    const authorContainer = document.querySelector('#authors');
+
+    /* creating the divs that the books will go into */
+    let newBookTitleSlot = document.createElement('div');
+    let newBookAuthorSlot = document.createElement('div');
+    newBookTitleSlot.classList.add('book');
+    newBookAuthorSlot.classList.add('book');
+    /* creating the element for displaying the name and author of the book */
     let newBookTitle = document.createElement('p');
     newBookTitle.innerText = myLibrary[0];
     let newBookAuthor = document.createElement('p');
     newBookAuthor.innerText = myLibrary[1];
-    newBookSlot.appendChild(newBookTitle);
-    newBookSlot.appendChild(newBookAuthor);
-    container.appendChild(newBookSlot);
+
+    newBookTitleSlot.appendChild(newBookTitle);
+    newBookAuthorSlot.appendChild(newBookAuthor);
+    authorContainer.appendChild(newBookAuthorSlot);
+    titleContainer.appendChild(newBookTitleSlot);
+    
     console.log(myLibrary);
 }
 
