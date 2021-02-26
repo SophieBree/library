8995a70b8a356813fd9072b6976a494b5c22e67d
Sophie Lewis <sophielewis@protonmail.com>
2021-02-26 18:04:46 +1100

    Fixed pin and read signs so they properly persist

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-// declare empty library array
 let myLibrary = localStorage.getItem("books")
   ? JSON.parse(localStorage.getItem("books"))
   : [];
 console.log(myLibrary);
-// book constructor
+
+const bookContainer = document.querySelector("#library");
+
 class Book {
   constructor(id, title, author, publisher, read, pinned) {
     this.id = id;
@@ -20,10 +21,14 @@ class Book {
     this.pinned = !this.pinned;
   }
 }
-myLibrary.forEach((value, index) => {
-  pullFromStorage(index);
-});
-// function for adding books
+let bookid;
+let bookTitle;
+let bookAuthor;
+let bookPublisher;
+let bookRead;
+let bookPinned;
+pullFromStorage();
+
 function addBookToLibrary() {
   if (
     document.querySelector('input[name="title"]').value === "" ||
@@ -31,9 +36,7 @@ function addBookToLibrary() {
   ) {
     return null;
   }
-
   myLibrary.length === 0 ? (bookid = 1) : (bookid = myLibrary.length + 1);
-
   bookTitle = document.querySelector('input[name="title"]').value;
   bookAuthor = document.querySelector('input[name="author"]').value;
   bookPublisher = document.querySelector('input[name="publisher"]').value;
@@ -45,7 +48,7 @@ function addBookToLibrary() {
       bookTitle === myLibrary[index].title &&
       bookAuthor === myLibrary[index].author
     ) {
-      alert("You already have this book.");
+      alert("You already have a book with this title and author.");
     }
   });
 
@@ -60,51 +63,57 @@ function addBookToLibrary() {
 
   myLibrary.push(newBook);
   localStorage.setItem("books", JSON.stringify(myLibrary));
-  showBooks();
-
   document.querySelector(".form-container").reset();
-  // console.log(myLibrary);
+  console.log(myLibrary);
+  showBook();
 }
 
-function showBooks() {
-  const bookContainer = document.querySelector("#library");
+function showBook(index) {
   let newBookSlot = document.createElement("div");
   newBookSlot.classList.add("book");
-  newBookSlot.dataset.id = bookid;
+  let books = document.querySelectorAll(".book");
+  myLibrary[index] === undefined
+    ? (newBookSlot.dataset.id = myLibrary.length)
+    : (newBookSlot.dataset.id = myLibrary[index].id);
 
   //row 1
-  idNumber = document.createElement("div");
+  let idNumber = document.createElement("div");
   idNumber.classList.add("id-number");
   let deleteButton = document.createElement("div");
   deleteButton.classList.add("delete");
-  idNumber.innerText = bookid;
-  idNumber.dataset.id = bookid;
+  myLibrary[index] === undefined
+    ? ((idNumber.innerText = myLibrary.length),
+      (idNumber.dataset.id = myLibrary.length))
+    : ((idNumber.innerText = myLibrary[index].id),
+      (idNumber.dataset.id = myLibrary[index].id));
+
   deleteButton.innerText = "X";
-  deleteButton.dataset.id = bookid;
+  myLibrary[index] === undefined
+    ? (deleteButton.dataset.id = myLibrary.length)
+    : (deleteButton.dataset.id = myLibrary[index].id);
+
   deleteButton.onclick = () => {
-    if (newBookSlot.dataset.id === deleteButton.dataset.id) {
-      myLibrary.splice(deleteButton.dataset.id - 1, 1);
-      newBookSlot.remove();
+    myLibrary.splice(deleteButton.dataset.id - 1, 1);
+    newBookSlot.remove();
 
-      myLibrary.forEach((value, index) => {
-        myLibrary[index].id = index + 1;
-      });
-      let books = document.querySelectorAll(".book");
-      books.forEach((value, index) => {
-        books[index].dataset.id = index + 1;
-      });
-      let deleteButtons = document.querySelectorAll(".delete");
-      deleteButtons.forEach((value, index) => {
-        deleteButtons[index].dataset.id = index + 1;
-      });
-      let idNumbers = document.querySelectorAll(".id-number");
-      idNumbers.forEach((value, index) => {
-        idNumbers[index].dataset.id = index + 1;
-        idNumbers[index].innerText = idNumbers[index].dataset.id;
-      });
-
-      console.log(myLibrary);
-    }
+    myLibrary.forEach((value, index) => {
+      myLibrary[index].id = index + 1;
+      console.log(myLibrary)
+    });
+    let books = document.querySelectorAll(".book");
+    books.forEach((value, index) => {
+      books[index].dataset.id = index + 1;
+    });
+    let deleteButtons = document.querySelectorAll(".delete");
+    deleteButtons.forEach((value, index) => {
+      deleteButtons[index].dataset.id = index + 1;
+    });
+    let idNumbers = document.querySelectorAll(".id-number");
+    idNumbers.forEach((value, index) => {
+      idNumbers[index].dataset.id = index + 1;
+      idNumbers[index].innerText = idNumbers[index].dataset.id;
+    });
+    localStorage.setItem("books", JSON.stringify(myLibrary));
   };
 
   newBookSlot.appendChild(idNumber);
@@ -114,53 +123,91 @@ function showBooks() {
   let newBookTitleSlot = document.createElement("div");
   newBookTitleSlot.classList.add("bookinfo", "title");
   let newBookTitle = document.createElement("p");
-  newBookTitle.innerText = bookTitle;
+  myLibrary[index] === undefined
+    ? (newBookTitle.innerText = myLibrary[myLibrary.length - 1].title)
+    : (newBookTitle.innerText = myLibrary[index].title);
   newBookTitleSlot.appendChild(newBookTitle);
 
   //row 3
   let newBookAuthorSlot = document.createElement("div");
   newBookAuthorSlot.classList.add("bookinfo", "author");
   let newBookAuthor = document.createElement("p");
-  newBookAuthor.innerText = bookAuthor;
+  myLibrary[index] === undefined
+    ? (newBookAuthor.innerText = myLibrary[myLibrary.length - 1].author)
+    : (newBookAuthor.innerText = myLibrary[index].author);
   newBookAuthorSlot.appendChild(newBookAuthor);
 
   //row 4
   let newBookPublisherSlot = document.createElement("div");
   newBookPublisherSlot.classList.add("bookinfo", "publisher");
   let newBookPublisher = document.createElement("p");
-  newBookPublisher.innerText = bookPublisher;
+  myLibrary[index] === undefined
+    ? (newBookPublisher.innerText = myLibrary[myLibrary.length - 1].publisher)
+    : (newBookPublisher.innerText = myLibrary[index].publisher);
   newBookPublisherSlot.appendChild(newBookPublisher);
 
   //row 5
   let pinButton = document.createElement("div");
-  pinButton.classList.add('pinbutton')
+  pinButton.classList.add("pinbutton");
   let pinImg = document.createElement("img");
   pinImg.src = "pin.png";
   pinButton.appendChild(pinImg);
+  
   pinButton.onclick = () => {
-    pinButton.parentElement.classList.contains("pinned")
-      ? (pinButton.parentElement.classList.remove("pinned"),
-        (pinButton.style.backgroundColor = ""))
-      : (pinButton.parentElement.classList.add("pinned"),
-        (pinButton.style.backgroundColor = "green"));
-  };
+    if (myLibrary[index] === undefined) {
+      if (newBook.pinned === false) {
+        newBook.pinned = true;
+        pinButton.parentElement.classList.add("pinned");
+        pinButton.style.backgroundColor = "green";
+        localStorage.setItem("books", JSON.stringify(myLibrary));
+        console.log(myLibrary);
+      } else {
+        newBook.pinned = false;
+        pinButton.parentElement.classList.remove("pinned");
+        pinButton.style.backgroundColor = "";
+        localStorage.setItem("books", JSON.stringify(myLibrary));
+        console.log(myLibrary);
+      }
+  } else {
+    if (myLibrary[index].pinned === false) {
+      myLibrary[index].pinned = true;
+      pinButton.parentElement.classList.add("pinned");
+      pinButton.style.backgroundColor = "green";
+      localStorage.setItem("books", JSON.stringify(myLibrary));
+      console.log(myLibrary);
+    } else {
+      myLibrary[index].pinned = false;
+      pinButton.parentElement.classList.remove("pinned");
+      pinButton.style.backgroundColor = "";
+      localStorage.setItem("books", JSON.stringify(myLibrary));
+      console.log(myLibrary);
+    }
+  }
+}
 
   let readButtonSlot = document.createElement("div");
-  readButtonSlot.setAttribute("id", "readbuttonslot");
+  readButtonSlot.classList.add("read-button-slot");
   readButtonSlot.innerText = "Read?";
   let readCheckbox = document.createElement("input");
   readCheckbox.type = "checkbox";
   readCheckbox.name = "read";
-  readCheckbox.value = "value";
+  if (myLibrary[index].read === true) {
+    readCheckbox.checked = true;
+  } else {
+    readCheckbox.checked = false;
+  }
+
   readCheckbox.onchange = () => {
     readCheckbox.checked === true
       ? (myLibrary[readButtonSlot.parentElement.dataset.id - 1].read = true)
       : (myLibrary[readButtonSlot.parentElement.dataset.id - 1].read = false);
+
     readButtonSlot.parentElement.classList.contains("read")
       ? readButtonSlot.parentElement.classList.remove("read")
       : readButtonSlot.parentElement.classList.add("read");
-    console.log(myLibrary);
+    localStorage.setItem("books", JSON.stringify(myLibrary));
   };
+
   readButtonSlot.appendChild(readCheckbox);
 
   newBookSlot.appendChild(newBookTitleSlot);
@@ -169,6 +216,25 @@ function showBooks() {
   newBookSlot.appendChild(pinButton);
   newBookSlot.appendChild(readButtonSlot);
   bookContainer.appendChild(newBookSlot);
+
+  myLibrary.forEach((value, index) => {
+    let books = document.querySelectorAll('.book');
+      books.forEach((value, index) => {
+        if (myLibrary[index].pinned === true) {
+          books[index].classList.add('pinned');
+          books[index].childNodes[5].style.backgroundColor = 'green'
+      } else {
+          books[index].classList.remove('pinned');
+          books[index].childNodes[5].style.backgroundColor = ''
+        }
+
+        if (myLibrary[index].read === true) {
+          books[index].classList.add('read');
+        } else {
+          books[index].classList.remove('read');
+        }
+      })
+  })
 }
 
 function openForm() {
@@ -211,146 +277,8 @@ function openRead() {
   });
 }
 
-function pullFromStorage(index) {
-  const bookContainer = document.querySelector("#library");
-  let newBookSlot = document.createElement("div");
-  newBookSlot.classList.add("book");
-  let books = document.querySelectorAll(".book");
-  books.forEach((value, index) => {
-    if (myLibrary[index].read === true) {
-      books[index].classList.add("read");
-    }
+function pullFromStorage() {
+  myLibrary.forEach((value, index) => {
+    showBook(index);
   });
-  newBookSlot.dataset.id = myLibrary[index].id;
-  myLibrary.length === 0 ? (bookid = 1) : (bookid = myLibrary[index].id);
-
-  bookTitle = myLibrary[index].title;
-  bookAuthor = myLibrary[index].author;
-  bookPublisher = myLibrary[index].publisher;
-  bookRead = false;
-
-  idNumber = document.createElement("div");
-  idNumber.classList.add("id-number");
-  let deleteButton = document.createElement("div");
-  deleteButton.classList.add("delete");
-  idNumber.innerText = bookid;
-  idNumber.dataset.id = bookid;
-  deleteButton.innerText = "X";
-  deleteButton.dataset.id = bookid;
-  deleteButton.onclick = () => {
-    if (newBookSlot.dataset.id === deleteButton.dataset.id) {
-      myLibrary.splice(deleteButton.dataset.id - 1, 1);
-      newBookSlot.remove();
-
-      myLibrary.forEach((value, index) => {
-        myLibrary[index].id = index + 1;
-      });
-      let books = document.querySelectorAll(".book");
-      books.forEach((value, index) => {
-        books[index].dataset.id = index + 1;
-      });
-      let deleteButtons = document.querySelectorAll(".delete");
-      deleteButtons.forEach((value, index) => {
-        deleteButtons[index].dataset.id = index + 1;
-      });
-      let idNumbers = document.querySelectorAll(".id-number");
-      idNumbers.forEach((value, index) => {
-        idNumbers[index].dataset.id = index + 1;
-        idNumbers[index].innerText = idNumbers[index].dataset.id;
-      });
-
-      console.log(myLibrary);
-    }
-  };
-
-  newBookSlot.appendChild(idNumber);
-  newBookSlot.appendChild(deleteButton);
-
-  //row 2
-  let newBookTitleSlot = document.createElement("div");
-  newBookTitleSlot.classList.add("bookinfo", "title");
-  let newBookTitle = document.createElement("p");
-  newBookTitle.innerText = bookTitle;
-  newBookTitleSlot.appendChild(newBookTitle);
-
-  //row 3
-  let newBookAuthorSlot = document.createElement("div");
-  newBookAuthorSlot.classList.add("bookinfo", "author");
-  let newBookAuthor = document.createElement("p");
-  newBookAuthor.innerText = bookAuthor;
-  newBookAuthorSlot.appendChild(newBookAuthor);
-
-  //row 4
-  let newBookPublisherSlot = document.createElement("div");
-  newBookPublisherSlot.classList.add("bookinfo", "publisher");
-  let newBookPublisher = document.createElement("p");
-  newBookPublisher.innerText = bookPublisher;
-  newBookPublisherSlot.appendChild(newBookPublisher);
-
-  //row 5
-  let pinButton = document.createElement("div");
-  pinButton.classList.add("pinbutton");
-  let pinImg = document.createElement("img");
-  pinImg.src = "pin.png";
-  pinButton.appendChild(pinImg);
-  console.log(books);
-  if (myLibrary[index].pinned === true) {
-      books.forEach((value, index) => {
-        books[index].classList.add('pinned');
-        books[index].children[5].style.backgroundColor = 'green'
-      })
-    
-} 
-
-  pinButton.onclick = () => {
-    pinButton.parentElement.classList.contains("pinned")
-      ? (pinButton.parentElement.classList.remove("pinned"),
-        (pinButton.style.backgroundColor = ""),
-        (myLibrary[pinButton.parentElement.dataset.id - 1].pinned = false))
-      : (pinButton.parentElement.classList.add("pinned"),
-        (pinButton.style.backgroundColor = "green"),
-        (myLibrary[pinButton.parentElement.dataset.id - 1].pinned = true));
-    localStorage.setItem("books", JSON.stringify(myLibrary));
-  };
-
-  let readButtonSlot = document.createElement("div");
-  readButtonSlot.setAttribute("id", "readbuttonslot");
-  readButtonSlot.innerText = "Read?";
-  let readCheckbox = document.createElement("input");
-  readCheckbox.type = "checkbox";
-  readCheckbox.name = "read";
-  readCheckbox.checked = "false";
-  if (myLibrary[index].read === true) {
-    readCheckbox.checked = true;
-  } else {
-    readCheckbox.checked = false;
-  }
-  readCheckbox.onchange = () => {
-    readCheckbox.checked === true
-      ? (myLibrary[readButtonSlot.parentElement.dataset.id - 1].read = true)
-      : (myLibrary[readButtonSlot.parentElement.dataset.id - 1].read = false);
-    readButtonSlot.parentElement.classList.contains("read")
-      ? readButtonSlot.parentElement.classList.remove("read")
-      : readButtonSlot.parentElement.classList.add("read");
-    localStorage.setItem("books", JSON.stringify(myLibrary));
-    console.log(myLibrary);
-  };
-  readButtonSlot.appendChild(readCheckbox);
-
-  newBookSlot.appendChild(newBookTitleSlot);
-  newBookSlot.appendChild(newBookAuthorSlot);
-  newBookSlot.appendChild(newBookPublisherSlot);
-  newBookSlot.appendChild(pinButton);
-  newBookSlot.appendChild(readButtonSlot);
-  bookContainer.appendChild(newBookSlot);
 }
-/*
-function deleteAllBooks() {
-    let library = document.querySelector('#library')
-    while (library.firstChild) {
-        library.removeChild(library.firstChild)
-    }
-    localStorage.clear();
-    
-}
-*/
